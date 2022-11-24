865bd8d2edda3e1d4c0db0b79b41a1882a7a4f88
MemoryColor <116071608+MemoryColor@users.noreply.github.com>
2022-11-24 01:28:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ class Simon {
             startButton,
             round
         }
-        this.errorSound = new Audio('./sounds/error.mp3');
-        this.winSound = new Audio('./sounds/win.mp3')
+        this.errorSound = new Audio('./sounds/error.wav');
+        this.winSound = new Audio('./sounds/win.wav')
         this.buttonSounds = [
-            new Audio('./sounds/1.mp3'),
-            new Audio('./sounds/2.mp3'),
-            new Audio('./sounds/3.mp3'),
-            new Audio('./sounds/4.mp3'),
+            new Audio('./sounds/1.wav'),
+            new Audio('./sounds/2.wav'),
+            new Audio('./sounds/3.wav'),
+            new Audio('./sounds/4.wav'),
         ]
     }
 
